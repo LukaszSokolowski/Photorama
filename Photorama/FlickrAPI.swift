3cca74a50564bb777816337bb0d5bd8c76d8bf48
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-08-18 20:44:52 +0200

    Attached swift file with FlickrAPI struct.

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Łukasz Sokołowski on 18/08/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrAPI {
+    
+}
