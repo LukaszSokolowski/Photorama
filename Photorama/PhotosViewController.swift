24ef53785d6e385bae768b0107e1fc22e80da159
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-08-18 20:23:56 +0200

    Created PhotosViewController class. Added image view and navigation controller.

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Łukasz Sokołowski on 18/08/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import Foundation
