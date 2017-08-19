b39893f78a44d3ac9d06a77517914bc4e5d67af9
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-08-19 18:01:08 +0200

    PhotoStore class created.

---
+++ Photorama/PhotoStore.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Łukasz Sokołowski on 19/08/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import Foundation
+
+class PhotoStore {
+    
+}
