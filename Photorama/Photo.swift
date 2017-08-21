d6272be0a27a80866964974feac96005c35f997f
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-08-21 19:38:23 +0200

    Added Photo class which is responsible for representing each photo from service.

---
+++ Photorama/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Łukasz Sokołowski on 21/08/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+}
