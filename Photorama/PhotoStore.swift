632de3153dfbf2f933986042c9521a9d4f90b82c
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-08-22 15:05:09 +0200

    fetchInterestingPhotos method update.

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -27,22 +27,8 @@ class PhotoStore {
         let task = session.dataTask(with: request) {
             (data,response,error) -> Void in
             
-            if let jsonData = data {
-            do {
-                
-                let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                    print(jsonObject)
-                
-            } catch let error {
-                    print("Error creating JSON object: \(error)")
-                }
-            } else if let requestError = error {
-                    print("Error fetching interesting photos: \(requestError)")
-            }
-               else {
-                  print("Unexpected error with the request")
-            }
-       }
+                  let result = self.processPhotosRequest(data: data, error: error)
+        }
         task.resume()
     }
     
