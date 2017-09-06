3c83d3ae453c832ae74bdf543f1ed691ca320225
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-09-07 01:00:00 +0200

    Creating a custom collection view cell.

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Łukasz Sokołowski on 07/09/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+}
