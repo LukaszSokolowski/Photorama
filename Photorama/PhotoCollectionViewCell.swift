9b15c43ac6becb7f785c4b50b6adcdf91417797d
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-09-08 21:16:30 +0200

    Configuring the activity indicator.

--- Photorama/PhotoCollectionViewCell.swift
+++ Photorama/PhotoCollectionViewCell.swift
@@ -12,6 +12,15 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var spinner: UIActivityIndicatorView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+    
     func update(with image: UIImage?) {
         if let imageToDisplay = image {
             spinner.stopAnimating()
