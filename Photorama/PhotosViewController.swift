82819b4a0bd980de15eb56c9fe8ac7eacd3cdfa1
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-09-09 21:54:03 +0200

    Implementation of collection view delegate method which take care about displaying the cell.

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -35,5 +35,29 @@ class PhotosViewController: UIViewController,UICollectionViewDelegate {
             self.collectionView.reloadSections(IndexSet(integer: 0))
          }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let photo = photoDataSource.photos[indexPath.row]
+        // Download the image data, which could take some time
+        
+        store.fetchImage(for: photo) { (result) -> Void in
+            // The index path for the photo might have changed between the
+            // time the request started and finished, so find the most
+            // recent index path
+            
+            guard let photoIndex = self.photoDataSource.photos.index(of: photo),
+                case let .success(image) = result else {
+                    return
+            }
+            let photoIndexPath = IndexPath(item: photoIndex, section: 0)
+            // When the request finishes, only update the cell if it's still visible
+            if let cell = self.collectionView.cellForItem(at: photoIndexPath) as? PhotoCollectionViewCell {
+
+            cell.update(with: image)
+            }
+        }
+    }
+    
 }
 
